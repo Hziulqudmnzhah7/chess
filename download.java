d7fe610bf269f5253a051bcb8365eb1b64bd2079
Hziulqudmnzhah7 <31997303+Hziulqudmnzhah7@users.noreply.github.com>
2017-11-01 21:39:57 -0400

    test file for latter implementation

---
+++ download.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+public class download{
+   public static void main(String[] args){
+      try {
+         URL url = new URL("https://raw.githubusercontent.com/Hziulqudmnzhah7/chess/changes/chess.java");
+		   URLConnection conn = url.openConnection();
+		   BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+         String response = new String();
+         for (String line; (line = br.readLine()) != null; response += line);
+            System.out.println(response);
+      } catch(MalformedURLException e){
+         System.out.println("something happened");
+      }catch (IOException e){
+         System.out.println("something happened");
+      }
+   }
+}
