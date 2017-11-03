b7c75cd22239351a4563349a245dcdabcf079c97
Hziulqudmnzhah7 <31997303+Hziulqudmnzhah7@users.noreply.github.com>
2017-11-03 12:02:20 -0400

    Update download.java

--- download.java
+++ download.java
@@ -4,8 +4,8 @@ public class download{
    public static void main(String[] args){
       try {
          URL url = new URL("https://raw.githubusercontent.com/Hziulqudmnzhah7/chess/changes/chess.java");
-		   URLConnection conn = url.openConnection();
-		   BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	URLConnection conn = url.openConnection();
+	BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
          String response = new String();
          for (String line; (line = br.readLine()) != null; response += line);
             System.out.println(response);
